fb86b4f0c512afdd5a8dcc2c3baf2df05a29cd69
thahira97 <thahirasheerin3@gmail.com>
2022-12-17 15:12:42 -0500

    Refactored codes uding function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,28 +1,23 @@
 const request = require("request");
-let input = process.argv.slice(2);
-// request(
-//   "https://api.thecatapi.com/v1/breeds/search?q=Siberian",
-//   (error, response, body) => {
-//     console.log("error:", error);
-//     console.log("statusCode:", response && response.statusCode);
-//     const data = JSON.parse(body);
-//     console.log(data);
-//     console.log(typeof data);
-//     console.log(data[0]["description"]);
-//   }
-// );
-request(
-  "https://api.thecatapi.com/v1/breeds/search?q=" + input,
-  (error, response, body) => {
-    if (error) {
-      console.log("Broken URL");
-    } else {
+
+const fetchBreedDescription = function (breed, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breed}`,
+    (error, _response, body) => {
       const data = JSON.parse(body);
       if (data.length === 0) {
-        console.log("Oops breed not found");
-      } else {
-        console.log(data[0].description);
+        callback(null, null);
+        return;
       }
+
+      if (error) {
+        callback(error, null);
+        return;
+      }
+
+      callback(null, data[0].description);
     }
-  }
-);
+  );
+};
+
+module.exports = { fetchBreedDescription };
